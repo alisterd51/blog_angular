4dc4487720fca002492f2c03245fc4a48b9a5b96
antoine clarman <antoinereims28@gmail.com>
2023-05-29 04:49:18 +0200

    add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -21,6 +21,7 @@ import { HomeComponent } from './home/home.component';
 import { TetrisComponent } from './tetris/tetris.component';
 import { FooterComponent } from './footer/footer.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -46,6 +47,12 @@ import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.componen
     MatTreeModule,
     MatSidenavModule,
     NgIconsModule.withIcons({ bootstrapGithub }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
