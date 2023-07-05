ee95ba425166e3af803a76ce4a7dc4bac1357d7d
alisterd51 <antoinereims28@gmail.com>
2023-07-05 02:26:32 +0200

    fix ng test

--- src/app/article/article.component.spec.ts
+++ src/app/article/article.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ArticleComponent } from './article.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ArticleComponent', () => {
   let component: ArticleComponent;
@@ -9,7 +10,7 @@ describe('ArticleComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [HttpClientModule, RouterTestingModule],
       declarations: [ArticleComponent]
     });
     fixture = TestBed.createComponent(ArticleComponent);
